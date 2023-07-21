7cb117182b5262900bff3dc4646dd14f839395da
Hugi34 <leyvadamian029@gmail.com>
2023-07-20 21:40:17 -0500

    fix: Spring boot would sometimes not load html file
    Sometimes spring boot would not run the html file, causing a fault. Fixed by changing Rest controller to normal controller.

--- discord_clone/src/main/java/com/backend/discord_clone/DiscordCloneController.java
+++ discord_clone/src/main/java/com/backend/discord_clone/DiscordCloneController.java
@@ -2,16 +2,17 @@ package com.backend.discord_clone;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
-@RestController
+@Controller
 public class DiscordCloneController {
 
-    @RequestMapping("/index")
-    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	@RequestMapping("/index")
+	public String index(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
 		model.addAttribute("name", name);
 		return "index";
 	}
-    
+
 }
