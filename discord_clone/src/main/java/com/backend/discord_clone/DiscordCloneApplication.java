388a98302ccb1209a2b50d41ca37e1d13a5a09a6
Hugi34 <leyvadamian029@gmail.com>
2023-07-20 20:50:17 -0500

    feat: adding in java spring boot (simple hello world output)
    added in spring boot with working html file reading in. Need to revise front end side into maven format.

---
+++ discord_clone/src/main/java/com/backend/discord_clone/DiscordCloneApplication.java
@@ -0,0 +1,13 @@
+package com.backend.discord_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordCloneApplication.class, args);
+	}
+
+}
---
+++ discord_clone/src/main/java/com/backend/discord_clone/DiscordCloneController.java
@@ -0,0 +1,17 @@
+package com.backend.discord_clone;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+
+@RestController
+public class DiscordCloneController {
+
+    @RequestMapping("/index")
+    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "index";
+	}
+    
+}
