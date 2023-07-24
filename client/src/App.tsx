37b6f6b534e8a7929d5da55b8015aed1084f466f
Ayo Omotosho <aomotosho4@gmail.com>
2023-07-24 16:51:23 -0500

    fix: css error swap to sass

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="test">
+        <div className="test2"></div>
+        <p>yo what up</p>
+      </div>
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
