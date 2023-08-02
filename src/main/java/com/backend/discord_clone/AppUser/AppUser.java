019447815f1122f70605fb82b0220302c8b715b9
Hugi34 <leyvadamian029@gmail.com>
2023-08-01 22:32:30 -0500

    feat: AppUser Class and database
    Created and added secure AppUser, AppUserRole, and implemented database with yml file.

---
+++ src/main/java/com/backend/discord_clone/AppUser/AppUser.java
@@ -0,0 +1,70 @@
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Enumerated;
+
+class AppUser implements UserDetails{
+
+    private Long id;
+    private String name;
+    private String userName;
+    private String email;
+    private String password;
+    @Enumerated
+    private AppUserRole appUserRole;
+    private Boolean locked;
+    private Boolean enabled;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(AppUserRole.name);
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
+    }
+
+
+
+
+    
+}
