f141996d5402bba9c1a10acc92fa87f8aad4b103
Hugi34 <leyvadamian029@gmail.com>
2023-08-03 20:27:04 -0500

    Feat: Starting UserRegistration controller.
    UserRegistration handles Users that are trying to use the app that are not yet registered.

--- src/main/java/com/backend/discord_clone/AppUser/AppUserRepository.java
+++ src/main/java/com/backend/discord_clone/AppUser/AppUserRepository.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Provides User data by their email.
+ */
 @Repository
 @Transactional(readOnly = true)
 public interface AppUserRepository {
---
+++ src/main/java/com/backend/discord_clone/Registration/RegistrationController.java
@@ -0,0 +1,17 @@
+package com.backend.discord_clone.Registration;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+
+/**
+ * RegistrationController handles user registration.
+ */
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+    
+}
