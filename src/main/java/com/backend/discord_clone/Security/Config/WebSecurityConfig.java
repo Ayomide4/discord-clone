0ae705806003c46ab15e544649462eb8d3bcdbe6
Hugi34 <leyvadamian029@gmail.com>
2023-08-09 22:31:36 -0500

    Feat: WebSecurity with BCrypt
    Finished websecurity and added password encryption. Needs more testing.

--- src/main/java/com/backend/discord_clone/Security/Config/WebSecurityConfig.java
+++ src/main/java/com/backend/discord_clone/Security/Config/WebSecurityConfig.java
@@ -1,12 +1,27 @@
 package com.backend.discord_clone.Security.Config;
 
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-public class WebSecurityConfig{
+import com.backend.discord_clone.AppUser.AppUserService;
+
+import lombok.AllArgsConstructor;
+
+
+@Configuration
+@AllArgsConstructor
+@EnableWebFluxSecurity
+public class WebSecurityConfig extends WebSecurityConfiguration{
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable());
         //Protects endpoints at /api/<type>/register
@@ -19,12 +34,19 @@ public class WebSecurityConfig{
                     
     return http.build();
     }
+    
+   public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.authenticationProvider(daoAuthenticationProvider());
 
-   
-//overriding depencies
 
+}
 
+public DaoAuthenticationProvider daoAuthenticationProvider (){
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setPasswordEncoder(bCryptPasswordEncoder);
+    provider.setUserDetailsService(appUserService);
+    return provider;
+}
 
-    }
-
+}
 
