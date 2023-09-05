88a21e57c320e6d8fabe5316b969b5e27c491d3d
Ayo Omotosho <aomotosho4@gmail.com>
2023-09-04 23:45:38 -0500

    feat: finish topbar css

--- client/src/components/SocialDashboard.tsx
+++ client/src/components/SocialDashboard.tsx
@@ -1,8 +1,12 @@
+import SocialHub from "./SocialHub";
 import Topbar from "./Topbar";
 
 export default function SocialDashboard() {
-  return <div className="social-dash-container">
-    <Topbar/>
-    <div className="line"></div>
-  </div>;
+  return (
+    <div className="social-dash-container">
+      <Topbar />
+      <div className="line"></div>
+      <SocialHub />
+    </div>
+  );
 }
