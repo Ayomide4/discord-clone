6415b16d63034ad465536f963a69a038181bd9a1
Ayo Omotosho <aomotosho4@gmail.com>
2024-05-17 14:47:59 -0500

    add discord font

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -1,8 +1,10 @@
+import friendIcon from "../assets/result.svg";
 export default function Message() {
   return (
     <div className="message">
       <div className="message-header">
-        <p>Friend Name</p>
+        <img src={friendIcon} alt="friend-icon" className="friend-icon" />
+        <p>Friends</p>
       </div>
       <div className="conversation"></div>
       <div className="message-box"></div>
