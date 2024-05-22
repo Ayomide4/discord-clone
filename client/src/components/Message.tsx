04dfcbab1eb47ccd1d6e98d56fd92cc4e63b8266
Ayo Omotosho <aomotosho4@gmail.com>
2024-05-22 08:38:02 -0500

    finish friend activity

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -31,47 +31,55 @@ export default function Message() {
           />
         </div>
       </div>
-      <section className="conversation">
-        <div className="add-friend-container">
-          <h2>ADD FRIEND</h2>
-          <p>You can add friends with their Discord username.</p>
-          <div className="friend-request-container">
-            <input
-              type="text"
-              placeholder="You can add friends with their Discord username."
-              id="add-friend-input"
-            />
-            <button className="add-friend-btn">Send Friend Request</button>
-          </div>
-        </div>
-        <div className="extra">
-          <div className="img-container">
-            <div className="bg-img"></div>
-            <p id="bg-img-subtitle">
-              Wumpus is waiting of friends. You don't have to though!
-            </p>
-          </div>
-          <h2>OTHER PLACES TO MAKE FRIENDS</h2>
-          <div id="explore-btn">
-            <div id="explore-icon-container">
-              <img
-                src={exploreIcon}
-                alt="explore-icon"
-                id="explore-icon"
-                className="icon"
+      <div className="container">
+        <section className="conversation">
+          <div className="add-friend-container">
+            <h2>ADD FRIEND</h2>
+            <p>You can add friends with their Discord username.</p>
+            <div className="friend-request-container">
+              <input
+                type="text"
+                placeholder="You can add friends with their Discord username."
+                id="add-friend-input"
               />
+              <button className="add-friend-btn">Send Friend Request</button>
             </div>
-            <p>Explore Discoverable Servers</p>
-            <img src={rightChevron} alt="right-chevron" className="icon" />
           </div>
-        </div>
-      </section>
-
-      <div className="message-box"></div>
-      <section className="direct-message-container">
-        <div className="direct-message"></div>
-        <div className="friend-profile"></div>
-      </section>
+          <div className="extra">
+            <div className="img-container">
+              <div className="bg-img"></div>
+              <p id="bg-img-subtitle">
+                Wumpus is waiting of friends. You don't have to though!
+              </p>
+            </div>
+            <h2>OTHER PLACES TO MAKE FRIENDS</h2>
+            <div id="explore-btn">
+              <div id="explore-icon-container">
+                <img
+                  src={exploreIcon}
+                  alt="explore-icon"
+                  id="explore-icon"
+                  className="icon"
+                />
+              </div>
+              <p>Explore Discoverable Servers</p>
+              <img src={rightChevron} alt="right-chevron" className="icon" />
+            </div>
+          </div>
+        </section>
+        <section className="friend-activity">
+          <h2>Active Now</h2>
+          <div id="test">
+            <h3>It's quiet for now...</h3>
+            <div id="description-container">
+              <p>
+                When a friend starts an activity-like playing a game or hanging
+                out on voice-we'll show it here!
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
