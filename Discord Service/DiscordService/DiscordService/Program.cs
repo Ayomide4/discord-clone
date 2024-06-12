86d0ad472a3313c6e7352f88e6bf6496e4067228
hugi34 <Aa348841010$$>
2024-06-11 20:18:57 -0500

    LoginJWT Fix: Working JWT on successful login and controller to test on

--- Discord Service/DiscordService/DiscordService/Program.cs
+++ Discord Service/DiscordService/DiscordService/Program.cs
@@ -1,7 +1,11 @@
 using DiscordService.Db;
 using DiscordService.Interfaces;
 using DiscordService.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +17,25 @@ builder.Services.AddScoped<ILogin, LoginService>();
 builder.Services.AddScoped<IJwt, Jwt>();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DiscordSqlConnection")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var configuration = builder.Configuration;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = configuration["Jwt:Issuer"],
+
+            ValidateAudience = true,
+            ValidAudience = configuration["Jwt:Audience"],
+
+            ValidateLifetime = true,
+
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"]))
+        };
+    });
 
 var app = builder.Build();
 
